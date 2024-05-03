117d7a3eb4c13bbd199c79efd166f5b55cf67647
Siddharth <venkatesh.sidd@gmail.com>
2024-05-02 18:22:50 -0700

    responsiveness navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,23 +4,33 @@ import { Link } from "react-router-dom";
 import '../css/Nav.css';
 
 function Nav() {
-    return (
-        < nav class="navbar" >
-            <div id="trapezoid">
-                <div class="text1">
-                    <div><Link to="/">Home</Link></div>
-                    <div><Link to="/experience">Experiences</Link></div>
-                    <div><Link to="/projects">My Projects</Link></div>
-                    <div><Link to="/contact">Contact Me</Link></div>
-                </div>
-
-
                 {/*<a href="/about">About</a>*/}
                 {/*<button class="subnavbtn">Clients<i class="fa fa-caret-down"></i></button>*/}
                 {/*<button class="subnavbtn">Services<i class="fa fa-caret-down"></i></button>*/}
                 {/*<a href="https://codepen.io/pec-man" class="expandHome">Contact</a>*/}
+
+
+    return (
+        < nav class="navbar" >
+            
+            <div id="trapezoid">
+                <div class="dropdown">
+                    <div id="content" class="text1">
+                        <div><Link to="/">Home</Link></div>
+                        <div><Link to="/experience">Experiences</Link></div>
+                        <div><Link to="/projects">My Projects</Link></div>
+                        <div><Link to="/contact">Contact Me</Link></div>
+                    </div>
+                </div>
             </div>
+
         </nav>
+        
+
+
+
+
+
     );
 }
 
